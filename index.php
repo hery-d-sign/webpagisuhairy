1108fcb8fd1160ec6d80db28d7fc6e908c033021
hery-d-sign <thery87@gmail.com>
2020-02-12 15:12:14 +0700

    updetindex.php

--- index.php
+++ index.php
@@ -25,8 +25,211 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css.map">
 </head>
 <body>
+<!-- Syntac Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Tutup Syntac Navbar -->
+
+<div class="row">
+  <div class="col-md-3">
+  </div>
+  <div class="col-md-6">
+<!-- Syntax Slider -->
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="html/img/dark_patt.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://www.wallpaperflare.com/static/10/985/276/pattern-vector-dark-background-white-wallpaper.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://haksablog.files.wordpress.com/2018/12/Desain-tanpa-judul.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    </div>
+<!-- Tutup syntax Slider -->
+
+    </div>
+    <div class="col-md-3">
+    </div>
+    </div>
+    </div>
+
+<!-- open table -->
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<!-- close table -->
+
+  <div class="container-fluid">      
+    <!-- class row 1 -->
+    <div class="row">
+      <div class="col-md-3">
+      <!-- content -->
+        <h1>Haloo</h1>
+      </div>
+      <div class="col-md-3">
+      <!-- content -->
+        <h1>Hai</h1>
+      </div>
+      <div class="col-md-3">
+      <!-- content -->
+        <h1>Salam</h1>
+      </div>
+      <div class="col-md-3">
+      <!-- content -->
+        <h1>Kenal</h1>
+      </div>
+    </div>
+  </div>
+
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4 bg-info">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Footer Content</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="http://twitter.com/t_hery" target="blank">Twitter</a> 
+          </li>
+          <li>
+            <a href="http://instagram.com/t_hery87" target="blank">Instagram</a>
+          </li>
+          <li>
+            <a href="https://api.whatsapp.com/send?phone=6285260118387" target="blank">Whatsapp</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
 
     <!-- JS for bundle -->
+    <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.bundle.js"></script>
     <script src="assets/js/bootstrap.bundle.js.map"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
