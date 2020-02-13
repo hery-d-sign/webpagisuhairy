602585a5b7ef3afaca9516fa5e8f7e69f44e2d94
hery-d-sign <thery87@gmail.com>
2020-02-13 13:52:23 +0700

    perulangan.php

---
+++ perulangan_for.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        for ($i=0; $i<=10;$i++){
+            echo "Hallo <br> <br>";
+    }
+    ?>
+
+    <br>
+    <br>
+
+    <!-- contoh perulangan kedua angka genap -->
+    <?php
+    $i=2;
+        for($i;$i<=10;$i+=2){
+            echo "$i genap <br> <br>";
+        }
+    ?>
+    <!-- tutup contoh perulangan kedua angka genap -->
+
+    <br>
+    <br>
+
+    <!-- buka contoh perulangan angka ganjil -->
+
+    <?php
+    $i=1;
+        for($i;$i<=10;$i+=2){
+            echo "$i ganjil <br> <br>";
+        }
+    ?>
+    <!-- tutup contoh perulangan angka ganjil -->
+    <br>
+    <br>
+
+    <!-- contoh 1 -->
+    <?php
+    for($i=1; $i<=10; $i++){
+        echo "$i";
+    }
+    echo "<br> <br>";
+    ?>
+    <!-- tutup contoh 1 -->
+
+    <!-- contoh 2 -->
+    <?php
+    for($i=1; ;$i++){
+        if($i>10){
+        break;
+        }
+        echo "$i";
+    }
+    ?>
+    <!-- tutup contoh 2 -->
+
+    <!-- contoh 3 -->
+    <?php
+    $i=1;
+    for(; ;){
+        if($i>10){
+        break;
+        }
+        echo "$i";
+        $i++;
+    } echo "<br><br>";
+    ?>
+    <!-- tutup contoh 3 -->
+
+    <!-- contoh 4 -->
+    <?php
+    for($i=1; $i<=10; print "$i", $i++);
+    ?>
+    <!-- tutup contoh 4 -->
+    
+
+
+</body>
+</html>
