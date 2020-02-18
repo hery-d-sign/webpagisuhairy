380373ffc36e9be6991a9e4137af432b5dfa1417
hery-d-sign <thery87@gmail.com>
2020-02-18 12:01:18 +0700

    updetdiskonbootstrap.php

--- diskonbootstrap.php
+++ diskonbootstrap.php
@@ -2,42 +2,36 @@
     include 'header.php'
 ?>
 <div class="container">
-<form method="POST">
+<form action="" method="POST">
     <div class="form-group">
-        <label for="harga_barang">Harga Barang</label>
-        <input type="number" class="form-control col-sm-6"  name="harga_barang" aria-describedby="harga_barang">
-        <!-- <small id="emailHelp" class="form-text text-muted"></small> -->
+        <label for="nama_barang">Nama Barang :</label>
+        <input type="text" name="nama_brg" class="form-control col-sm-6" id="nama_barang">
     </div>
 
     <div class="form-group">
-        <label for="diskon">Diskon</label>
-        <input type="number" class="form-control col-sm-6" name="diskon">
+        <label for="harga_barang">Harga Barang :</label>
+        <input type="number" name="harga" class="form-control col-sm-6" id="harga_barang">
     </div>
-    <!-- <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div> -->
-        <button type="submit" name="tambah" class="btn btn-dark">Input</button>
+
+    <input type="submit" name="bayar" class="btn btn-dark" value="Bayar"></input>
 </form>
-<?php
-if (isset($_POST['tambah'])){
-    $harga_barang=$_POST['harga_barang'];
-    $diskon=$_POST['diskon'];
-    $syarat=400000;
-    $hargabeli=$harga_barang-$harga_barang*$diskon/100;
+</div>
 
-if($harga_barang>=$syarat){
-    echo " <div class='alert alert-success' role='alert'> $hargabeli";
-}
-    else{
-    echo " <div class='alert alert-danger' role='alert'> tidak mendapat diskon";
-}
-}
+<?php
+    if(isset($_POST['bayar'])){
+        $nama_barang=$_POST['nama_brg'];
+        $harga_barang=$_POST['harga'];
+        $diskon=50/100;
+        if ($harga_barang>=400000){
+            echo" 
+                <div class='alert alert-success' role='alert'> harga diskon yang diperoleh sebesar Rp. ", $harga_barang*$diskon;
+        
+            echo"
+            dan harga $nama_barang yang harus dibayar sebesar Rp.", $harga_barang-($harga_barang*$diskon);
+            echo" </div>";
+        }
+    }    
 ?>
 </div>
 
-
-<?php
-    include 'footer.php'
-?>
 
