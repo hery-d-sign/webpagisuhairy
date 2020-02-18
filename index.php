e70d970f602eb6f0524f0ca71cc2e2f5151c0673
hery-d-sign <thery87@gmail.com>
2020-02-18 08:28:04 +0700

    index.php

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-
-    <!-- css for bootstrap grid -->
-    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-grid.css.map">
-    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css.map">
-
-    <!-- css for bootstrap reboot -->
-    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css.map">
-    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css.map">
-
-    <!-- css for bootstrap -->
-    <link rel="stylesheet" href="assets/css/bootstrap.css"> 
-    <link rel="stylesheet" href="assets/css/bootstrap.css.map">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css.map">
-    
-</head>
-<body>
+<?php
+include'header.php';
+?>
 <!-- Syntac Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
@@ -256,19 +231,6 @@
   <!-- Copyright -->
 
 </footer>
-<!-- Footer -->
-
-    <!-- JS for bundle -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap.bundle.js"></script>
-    <script src="assets/js/bootstrap.bundle.js.map"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js.map"></script>
-
-    <!-- JS for Boostrap -->
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/bootstrap.js.map"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/bootstrap.min.js.map"></script>
-</body>
-</html>
+<?php
+include'footer.php';
+?>
