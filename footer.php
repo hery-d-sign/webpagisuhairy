8107fc3bc58be405c7309f520355aacecbfbafc5
hery-d-sign <thery87@gmail.com>
2020-02-18 12:01:50 +0700

    footer.php

--- footer.php
+++ footer.php
@@ -5,11 +5,13 @@
     <script src="assets/js/bootstrap.bundle.js.map"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js.map"></script>
+    <!-- close JS for bundle -->
 
-    <!-- JS for Boostrap -->
+    <!-- JS for Bootstrap -->
     <script src="assets/js/bootstrap.js"></script>
     <script src="assets/js/bootstrap.js.map"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/bootstrap.min.js.map"></script>
+    <!-- close JS for Bootstrap -->
 </body>
 </html>
