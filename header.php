3c795fcb3b7e69daee9072e0040680287f32a719
hery-d-sign <thery87@gmail.com>
2020-02-18 12:03:05 +0700

    updetheader.php

--- header.php
+++ header.php
@@ -11,18 +11,53 @@
     <link rel="stylesheet" href="assets/css/bootstrap-grid.css.map">
     <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css.map">
+    <!-- close css for bootstrap grid -->
 
     <!-- css for bootstrap reboot -->
     <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
     <link rel="stylesheet" href="assets/css/bootstrap-reboot.css.map">
     <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css.map">
+    <!-- close css for bootstrap reboot -->
 
     <!-- css for bootstrap -->
     <link rel="stylesheet" href="assets/css/bootstrap.css"> 
     <link rel="stylesheet" href="assets/css/bootstrap.css.map">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css.map">
+    <!-- close css for bootstrap -->
     
 </head>
 <body>
+
+<!-- Syntac Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-success">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Tutup Syntac Navbar -->
