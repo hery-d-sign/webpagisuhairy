632ae1f3d7994424f3617ebc9c8dd192416000db
hery-d-sign <thery87@gmail.com>
2020-02-18 08:26:46 +0700

    footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+    <!-- JS for bundle -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/bootstrap.bundle.js.map"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js.map"></script>
+
+    <!-- JS for Boostrap -->
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.js.map"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js.map"></script>
+</body>
+</html>
