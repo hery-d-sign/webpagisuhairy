efee19b4ed45d96a4a7b18941558252b0373eea6
hery-d-sign <thery87@gmail.com>
2020-02-21 16:26:25 +0700

    updatelogin_bootstrap.php

--- login_bootstrap.php
+++ login_bootstrap.php
@@ -6,12 +6,12 @@
     <title>Document</title>
 
     <?php
-        include 'header.php'
+        include 'header.php';
     ?>
 </head>
 <body>
     <div class="container">
-    <form action="" method="POST">
+    <form action="proses/proses_login.php" method="POST">
         <div class="form-group">
             <label for="username">User Name</label>
             <input type="text" name="username" class="form-control col-sm-2" id="username">
@@ -24,24 +24,6 @@
         <input type="submit" name="login" class="btn btn-success"></input>
     </form>
     </div>
-
-    <?php
-        if(isset($_POST['login'])){
-            $username=$_POST['username'];
-            $input_password=$_POST['password'];
-
-            if($username=='admin'&&$input_password=='admin123'){
-                header('location: latihanbiodata.php');
-            }else if($username=='siswa'&&$input_password=='siswa123'){
-                echo"<script>
-                alert(window.location.href='latihan.php');
-                </script>";
-            }else{
-                echo "<div class='alert alert-danger' role='alert'>Login Gagal</div>";
-            }
-
-        }
-    ?>
 
 
     <?php
