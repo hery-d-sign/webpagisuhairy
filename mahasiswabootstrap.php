2ac82e4a7c127a90182f05560511a8cdd8c3e68f
hery-d-sign <thery87@gmail.com>
2020-02-26 15:48:03 +0700

    mahasiswabootstrap.php

--- mahasiswabootstrap.php
+++ mahasiswabootstrap.php
@@ -1,7 +1,8 @@
 <?php
     include 'header.php'
 ?>
-    <form method="POST">
+    <form action="proses/proses_mahasiswa.php" method="POST">
+    <div class="container">
     <div class="form-group">
         <label for="nama">Nama</label>
         <input type="text" class="form-control col-sm-2"  name="nama" aria-describedby="nama">
@@ -37,50 +38,64 @@
         <label for="nilai uts">Nilai UTS</label>
         <input type="number" class="form-control col-sm-2" name="nilaiuts">
     </div>
-    
+
     <button type="submit" name="tambah" value="input" class="btn btn-danger">Input</button>
     </form>
-
-   <?php
-    include 'footer.php'
-   ?>
+    </div>
 
 <?php
-if (isset($_POST['tambah'])){
-    $nim=$_POST['nim'];
-    $nama=$_POST['nama'];
-    $jurusan=$_POST['jurusan'];
-    $harian=$_POST['nilaiharian'];
-    $quiz=$_POST['nilaiquiz'];
-    $uas=$_POST['nilaiuas'];
-    $uts=$_POST['nilaiuts'];
-    $nilai=($harian*10/100)+($quiz*15/100)+($uts*40/100)+($uas*35/100);
+    include 'footer.php'
+?>
+<br>
+
+<?php
+include 'koneksi/koneksi.php';
+
+$tampil_nilai=mysqli_query($koneksi, "SELECT * FROM mahasiswa")or die(mysqli($tampil_nilai));
+
+while($data=mysqli_fetch_array($tampil_nilai)){
+    if($data['nilai_akhir']<=50){
+        echo " <div class='alert alert-danger' role='alert'> Nama ".$data['nama_mahasiswa']." dengan Nim ".$data['nim_mahasiswa']." Jurusan ".$data['jurusan']." memperoleh nilai sebesar ".$data['nilai_akhir']." maka diperoleh grade : E <br></div>";
+    }
+    
+    else if($data['nilai_akhir']<=65){
+        echo "  <div class='alert alert-warning' role='alert'> Nama ".$data['nama_mahasiswa']." dengan Nim ".$data['nim_mahasiswa']." Jurusan ".$data['jurusan']." memperoleh nilai sebesar ".$data['nilai_akhir']." maka diperoleh grade : D </div>";
+    }
+    
+    else if($data['nilai_akhir']<=72){
+        echo "  <div class='alert alert-info' role='alert'> Nama ".$data['nama_mahasiswa']." dengan Nim ".$data['nim_mahasiswa']." Jurusan ".$data['jurusan']." memperoleh nilai sebesar ".$data['nilai_akhir']." maka diperoleh grade : C </div>";
+    }
+    
+    else if($data['nilai_akhir']<=83){
+        echo " <div class='alert alert-primary' role='alert'> Nama ".$data['nama_mahasiswa']." dengan Nim ".$data['nim_mahasiswa']." Jurusan ".$data['jurusan']." memperoleh nilai sebesar ".$data['nilai_akhir']." maka diperoleh grade : B </div>";
+    }
+    
+    else if($data['nilai_akhir']<=100){
+        echo " <div class='alert alert-success' role='alert'> Nama ".$data['nama_mahasiswa']." dengan Nim ".$data['nim_mahasiswa']." Jurusan ".$data['jurusan']." memperoleh nilai sebesar ".$data['nilai_akhir']." maka diperoleh grade : A </div>";
+    }
+    
+    else{
+        echo" <div class='alert alert-danger' role='alert'> $nama dengan $nim dan $jurusan dinyatakan tidak lulus </div>";
+    }
+
+}
+
+
+
+    
+
+// if (isset($_POST['tambah'])){
+//     $nim=$_POST['nim'];
+//     $nama=$_POST['nama'];
+//     $jurusan=$_POST['jurusan'];
+//     $harian=$_POST['nilaiharian'];
+//     $quiz=$_POST['nilaiquiz'];
+//     $uas=$_POST['nilaiuas'];
+//     $uts=$_POST['nilaiuts'];
+//     $nilai=($harian*10/100)+($quiz*15/100)+($uts*40/100)+($uas*35/100);
 
     // echo "nilai anda $nilai";
     // echo $contoh;
-}
+// }
 
-if($nilai<=50){
-    echo " <div class='alert alert-danger' role='alert'> Grade yang diperoleh oleh Nama $nama dengan Nim $nim dan Jurusan $jurusan nilai diperoleh $nilai adalah : E <br>";
-}
-
-else if($nilai <=65){
-    echo "  <div class='alert alert-warning' role='alert'> Grade yang diperoleh oleh Nama $nama dengan Nim $nim dan Jurusan $jurusan nilai diperoleh $nilai adalah : D";
-}
-
-else if($nilai <=72){
-    echo "  <div class='alert alert-primary' role='alert'>  Grade yang diperoleh oleh Nama $nama dengan Nim $nim dan Jurusan $jurusan nilai diperoleh $nilai adalah : C";
-}
-
-else if($nilai <=83){
-    echo " <div class='alert alert-success' role='alert'>  Grade yang diperoleh oleh Nama $nama dengan Nim $nim dan Jurusan $jurusan nilai diperoleh $nilai adalah : B";
-}
-
-else if($nilai <=100){
-    echo " <div class='alert alert-success' role='alert'> Grade yang diperoleh oleh Nama $nama dengan Nim $nim dan Jurusan $jurusan nilai diperoleh $nilai adalah : A";
-}
-
-else{
-    echo" <div class='alert alert-danger' role='alert'> $nama dengan $nim dan $jurusan dinyatakan tidak lulus";
-}
 ?>
