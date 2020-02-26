c9141a3408737a393d5960877582f029602a94a2
hery-d-sign <thery87@gmail.com>
2020-02-26 15:48:27 +0700

    koneksi.php

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -5,9 +5,9 @@ $password="";
 $db="barang";
 $koneksi=mysqli_connect($server, $username, $password, $db)or die(mysqli_error($koneksi));
 
-if($koneksi){
-    echo"database berhasil terkoneksi";
-}else if(!$koneksi){
-    die('koneksi gagal' .mysqli_connect_error());
-}
+// if($koneksi){
+//     echo"database berhasil terkoneksi";
+// }else if(!$koneksi){
+//     die('koneksi gagal' .mysqli_connect_error());
+// }
 ?>
