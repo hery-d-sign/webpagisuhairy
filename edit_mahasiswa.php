980705fdfddedb18b4ef99e83dc12fc127afd13e
hery-d-sign <thery87@gmail.com>
2020-02-27 15:26:09 +0700

    edit_mahasiswa.php

--- edit_mahasiswa.php
+++ edit_mahasiswa.php
@@ -6,13 +6,13 @@
     include 'koneksi/koneksi.php';
 
 
-$id= $_GET['id_mahasiswa'];
+$id=$_GET['id_mahasiswa'];
 
 $show=mysqli_query($koneksi, "SELECT * FROM mahasiswa WHERE id_mahasiswa='$id'") or die (mysqli_error($show));
 
 if(mysqli_num_rows($show)==0){
     echo '
-        <script>window.history.back(</script>
+        <script>window.history.back</script>
     ';
 }else{
     $data=mysqli_fetch_array($show);
@@ -20,44 +20,50 @@ if(mysqli_num_rows($show)==0){
 
 ?>
 
-<form action="proses/proses_mahasiswa.php" method="GET">
+
     <div class="container">
-        <div class="form-group">
-            <label for="nama">Nama</label>
-            <input type="text" class="form-control col-sm-2"  name="nama" aria-describedby="nama" value="<?php echo $data['nama']?>">
-        <!-- <small id="emailHelp" class="form-text text-muted"></small> -->
-        </div>
+        <form action="proses/proses_edit_mahasiswa.php" method="GET">
+        <input type="hidden" name="id_mahasiswa" value="<?php echo $data['id_mahasiswa']?>">
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control col-sm-2"  name="nama" value="<?php echo $data['nama_mahasiswa']?>">
+            </div>
 
-        <div class="form-group">
-            <label for="nim">Nim</label>
-            <input type="number" class="form-control col-sm-2" name="nim" value="<?php echo $data['nim']?>">
-        </div>
-    
-        <div class="form-group">
-            <label for="jurusan">Jurusan</label>
-            <input type="text" class="form-control col-sm-2" name="jurusan" value="<?php echo $data['jurusan']?>">
-        </div>
-    
-        <div class="form-group">
-            <label for="nilai harian">Nilai Harian</label>
-            <input type="number" class="form-control col-sm-2" name="nilaiharian" value="<?php echo $data['nilaiharian']?>">
-        </div>
+            <div class="form-group">
+                <label for="nim">Nim</label>
+                <input type="number" class="form-control col-sm-2" name="nim" value="<?php echo $data['nim_mahasiswa']?>">
+            </div>
 
-        <div class="form-group">
-            <label for="nilai quiz">Nilai Quiz</label>
-            <input type="number" class="form-control col-sm-2" name="nilaiquiz" value="<?php echo $data['nilaiquiz']?>">
-        </div>
+            <div class="form-group">
+                <label for="mata kuliah">Mata Kuliah</label>
+                <input type="text" class="form-control col-sm-2" name="mata_kuliah" value="<?php echo $data['mata_kuliah']?>">
+            </div>
+        
+            <div class="form-group">
+                <label for="jurusan">Jurusan</label>
+                <input type="text" class="form-control col-sm-2" name="jurusan" value="<?php echo $data['jurusan']?>">
+            </div>
+        
+            <div class="form-group">
+                <label for="nilai harian">Nilai Harian</label>
+                <input type="number" class="form-control col-sm-2" name="nilaiharian" value="<?php echo $data['nilai_harian']?>">
+            </div>
 
-        <div class="form-group">
-            <label for="nilai uas">Nilai UAS</label>
-            <input type="number" class="form-control col-sm-2" name="nilaiuas" value="<?php echo $data['nilaiuas']?>">
-        </div>
+            <div class="form-group">
+                <label for="nilai quiz">Nilai Quiz</label>
+                <input type="number" class="form-control col-sm-2" name="nilaiquiz" value="<?php echo $data['nilai_quiz']?>">
+            </div>
 
-        <div class="form-group">
-            <label for="nilai uts">Nilai UTS</label>
-            <input type="number" class="form-control col-sm-2" name="nilaiuts" value="<?php echo $data['nilaiuts']?>">
-        </div>
+            <div class="form-group">
+                <label for="nilai uas">Nilai UAS</label>
+                <input type="number" class="form-control col-sm-2" name="nilaiuas" value="<?php echo $data['nilai_uas']?>">
+            </div>
 
-    <input type="submit" name="update" class="btn btn-danger" value="update">
-</form>
+            <div class="form-group">
+                <label for="nilai uts">Nilai UTS</label>
+                <input type="number" class="form-control col-sm-2" name="nilaiuts" value="<?php echo $data['nilai_uts']?>">
+            </div>
+
+        <input type="submit" name="update" class="btn btn-success" value="update">
+        </form>
     </div>
