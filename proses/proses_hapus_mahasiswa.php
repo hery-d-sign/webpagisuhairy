b049c160ec2161dc65ff9e3607f406f24f23e0a3
hery-d-sign <thery87@gmail.com>
2020-02-27 15:26:46 +0700

    proses_hapus_mahasiswa.php

---
+++ proses/proses_hapus_mahasiswa.php
@@ -0,0 +1,17 @@
+<?php
+    include '../koneksi/koneksi.php';
+
+    if(isset($_GET['id_mahasiswa'])){
+
+        $id=$_GET['id_mahasiswa'];
+        $hapus=mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE id_mahasiswa='$id'")or die(mysqli_error($hapus));
+
+        if($hapus){ ?>
+        <script>
+        alert('data berhasil dihapus'); window.location.href="../mahasiswabootstrap.php"</script>';
+
+    <?php }else{?>
+        <script>alert('data gagal dihapus')</script>
+    
+<?php   }
+}  ?>
