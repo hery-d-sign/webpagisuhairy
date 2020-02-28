677f95a7decaedc53d6d3361071f55abda6b92cc
hery-d-sign <thery87@gmail.com>
2020-02-28 15:57:05 +0700

    proses_biodata.php

--- proses/proses_biodata.php
+++ proses/proses_biodata.php
@@ -1,24 +1,32 @@
 <?php
+include '../koneksi/koneksi.php';
 error_reporting(0);
+
 if (isset($_POST['tambah'])){
     $nama=$_POST['nama'];
+    $alamat=$_POST['alamat'];
+    $no_hp=$_POST['no_hp'];
+    $hobby=$_POST['hobby'];
+    $jenis_kelamin=$_POST['JK'];
     $email=$_POST['email'];
-    $Jeniskelamin=$_POST['JK'];
-    $Alamat=$_POST['alamat'];
-    $Tanggallahir=$_POST['lahir'];
-    $Tempatlahir=$_POST['tempat'];
-    $Nohp=$_POST['1234567891012'];
-    $Hobby1=$_POST['Hobby1'];
-    $Hobby2=$_POST['Hobby2'];
-    $Hobby3=$_POST['Hobby3'];
-    $Hobby4=$_POST['Hobby4'];
-    echo "nama saya adalah : $nama <br>"; 
-    echo "email saya adalah : $email <br>";
-    echo "saya seorang : $Jeniskelamin <br>";
-    echo "alamat saya : $Alamat <br>";
-    echo "tanggal lahir saya : $Tanggallahir <br>";
-    echo "tempat lahir : $Tempatlahir <br>";
-    echo "no hp saya : $Nohp <br>" ;
-    echo "Hobby : $Hobby1 $Hobby2 $Hobby3 $Hobby4";
+    $tanggal_lahir=$_POST['lahir'];
+    
+    $biodata=mysqli_query($koneksi, "insert into biodata values ('', '$nama', '$alamat', '$no_hp', '$hobby', '$tanggal_lahir', '$jenis_kelamin', '$email', '' )")or die(mysqli_query($biodata));
+
+    if($biodata){
+        echo "<script>alert('Biodata Berhasil di input'); 
+        window.location.href='../latihanbiodata.php';
+        </script>";
+    }
+
+    
+    // echo "nama saya adalah : $nama <br>"; 
+    // echo "email saya adalah : $email <br>";
+    // echo "saya seorang : $Jeniskelamin <br>";
+    // echo "alamat saya : $Alamat <br>";
+    // echo "tanggal lahir saya : $Tanggallahir <br>";
+    // echo "tempat lahir : $Tempatlahir <br>";
+    // echo "no hp saya : $Nohp <br>" ;
+    // echo "Hobby : $Hobby1 $Hobby2 $Hobby3 $Hobby4";
 }
 ?>
