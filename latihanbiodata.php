f4c9befc43287e995d32bd625cecd8e1b05a001d
hery-d-sign <thery87@gmail.com>
2020-02-28 15:56:07 +0700

    latihanbiodata.php

--- latihanbiodata.php
+++ latihanbiodata.php
@@ -4,32 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+        include 'header.php';
+    ?>
 </head>
 <body>
     <form action="proses/proses_biodata.php" method="POST" name="tambah">
-    <label>Nama</label>
-    <input type="text" name="nama" placeholder="masukan nama"> <br> <br>
-    <label>Email</label>    
-    <input type="email" name="email" placeholder="email@email"><br> <br>
-    <label>Jenis Kelamin</label>
-    <input type="radio" name="JK" value="Laki-laki">Laki-Laki;
-    <input type="radio" name="JK" value="Perempuan">Perempuan; <br> <br>
-    <label>Alamat</label>
-    <input type="textarea" name="alamat" placeholder="masukkan alamat"> <br> <br>
-    <label>Tanggal Lahir</label>
-    <input type="date" name="lahir"> <br> <br>
-    <label>Tempat Lahir</label>
-    <input type="text" name="tempat" placeholder="masukkan tempat lahir"> <br> <br>
-    <label>No Hp</label>
-    <input type="number" name="1234567891012" placeholder="masukkan no hp"> <br> <br>
-    <label>Hobby</label>
-    <input type="checkbox" name="Hobby1" value="Mancing">Mancing;
-    <input type="checkbox" name="Hobby2" value="Catur">Catur;
-    <input type="checkbox" name="Hobby3" value="Nonton">Nonton;
-    <input type="checkbox" name="Hobby4" value="Bulutangkis">Bulutangkis; <br> <br>
+    <div class="container">
+    <div class="form-group">
+        <label>Nama</label>
+        <input type="text" name="nama" class="form-control col-sm-2" placeholder="masukan nama">
+    </div>
+
+    <div class="form-group">
+        <label>Alamat</label>
+        <input type="textarea" name="alamat" class="form-control col-sm-2" placeholder="masukkan alamat">
+    </div>
+
+    <div class="form-group">
+        <label for="no hp">No Handphone</label>
+        <input type="number" class="form-control col-sm-2" name="no_hp" placeholder="masukkan no handphone">
+    </div>
+
+    <div class="form-group">
+        <label for="hobby">Hobby</label>
+        <input type="text" class="form-control col-sm-2" name="hobby" placeholder="masukkan hobby">   
+    </div>
+
+    <div class="form-group">
+        <label>Jenis Kelamin</label>
+        <input type="radio" name="JK" value="Laki-laki">Laki-Laki;
+        <input type="radio" name="JK" value="Perempuan">Perempuan;
+    </div>
+
+    <div class="form-group">
+        <label>Tanggal Lahir</label>
+        <input type="date" name="lahir" class="form-control col-sm-2">
+    </div>
+
+    <div class="form-group">
+        <label>Email</label>    
+        <input type="email" name="email" class="form-control col-sm-2" placeholder="email@email">
+    </div>
+    
+    <!-- <div class="form-group">
+        <label>Tempat Lahir</label>
+        <input type="text" name="tempat" class="form-control col-sm-2" placeholder="masukkan tempat lahir">
+    </div> -->
+
     <input type="submit" name="tambah" value="Submit">
     </form>
+    </div>
 
+<?php
+    include 'footer.php';
+?>
 </body>
 </html>
 
